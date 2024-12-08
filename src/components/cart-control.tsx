d79928b3d09855972ac57138b39f2a5debef281b
Leonardo <leonardo.palvarenga@gmail.com>
2024-12-08 03:43:48 -0300

    feat: update CartControl component to conditionally display item count

--- src/components/cart-control.tsx
+++ src/components/cart-control.tsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useCart } from "../redux/sliceCart";
 
-
 export function CartControl() {
-  const  productCart = useSelector(useCart);
+  const productCart = useSelector(useCart);
 
-  const total = productCart.length
+  const total = productCart.length;
 
   return (
-    <Link to={'/cart'} className="relative">
+    <Link to={"/cart"} className="relative">
       <IoCartOutline size={24} />
-      <span className="text-xs text-white rounded-full w-4 h-4 flex justify-center items-center bg-red-500 absolute -top-1 -right-1">
-        {total}
-      </span>
+      {total > 0 && (
+        <span className="text-xs text-white rounded-full w-4 h-4 flex justify-center items-center bg-red-500 absolute -top-1 -right-1">
+          {total}
+        </span>
+      )}
     </Link>
   );
 }
